77a299a24b31d16292f38cb5e95d1ddce15182f3
Anthony Suárez <neolight1010@gmail.com>
2022-06-12 15:29:12 -0500

    Create main menu.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 mod hangman;
 
+use cursive::views::{Dialog, TextView};
+
 use crate::hangman::Hangman;
 
 fn main() {
     let hangman = Hangman::new(&"hello".to_string());
 
-    println!("{:?}", &hangman);
+    let mut siv = cursive::default();
+
+    siv.add_layer(main_menu());
+
+    siv.run();
+}
+
+fn main_menu() -> impl cursive::View {
+    Dialog::around(TextView::new("Hangman Rust").center())
+        .button("Start", |s| {
+            s.pop_layer();
+            s.add_layer(game_screen());
+        })
+        .button("Quit", |s| s.quit())
+}
+
+fn game_screen() -> impl cursive::View {
+    TextView::new("My game screen!")
 }
